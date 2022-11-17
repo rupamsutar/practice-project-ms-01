ae037358512ccdd9b6942cabdcdbdcf06c259151
Rupam Sutar <rupamsutar281998@gmail.com>
2022-11-17 10:31:09 +0530

    Max's Project continued for further section

--- src/App.js
+++ src/App.js
@@ -1,35 +1,24 @@
 import React, { useState } from 'react';
-import AddUser from "./Components/AddUser/AddUser";
-import UserList from "./Components/UserList/UserList";
 
-
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
 
-  const [userList, setUserList] = useState([]);
-
-  const submitUserHandler = (userName, age) => {
-
-    console.log(userName, age);
-
-    setUserList( (prevState) => {
-      return (
-        [ ...userList, {Name: userName, Age: age, id: Math.random().toString()}]
-      )
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
     });
-  }
-
-  // console.log(userList);
-
+  };
 
   return (
-    <div>      
-      <AddUser
-        onSubmit={submitUserHandler} 
-      />
-      <UserList 
-        userList={userList}
-      />
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/Components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Button.css";
-
-const Button = (props) => {
-    return <button className="button" onClick={props.onSubmit}>Add User</button>
-}
-
-export default Button;
